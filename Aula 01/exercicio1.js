87f95c3f86192eba0902f6a03a371adace34abbe
Eduarda-Machado2026 <andradele.23@gmail.com>
2026-08-20 20:18:26 -0300

    exercios da aula 01

---
+++ Aula 01/exercicio1.js
@@ -0,0 +1,10 @@
+var idade= -10
+
+  if (idade>=0)
+  {
+    console.log('positivo')
+}
+{
+    console.log('negativo')
+    
+}
---
+++ Aula 01/exercicio5.js
@@ -0,0 +1,13 @@
+numero1=15
+numero2=10
+ if(numero1>numero2)
+ {
+    console.log(numero1, "é maior")
+ }
+ else if (numero2>numero1)
+ {
+    console.log(numero2, "é maior")
+ }
+ else{
+    console.log("os dois numeros são iguais")
+ };
