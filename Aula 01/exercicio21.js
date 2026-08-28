6049efb0453a3e742d4248b570f343faa53f164e
Eduarda-Machado2026 <andradele.23@gmail.com>
2026-08-27 21:05:46 -0300

    adicionar novo exercicio

---
+++ Aula 01/exercicio21.js
@@ -0,0 +1,10 @@
+function contarVogal(strng){
+    let vogais= "aeiouAEIOU";
+    let count=0;
+    for (let i=0;i<str.length;i++){
+        if(vogais.includes(str[i])){
+           
+        }
+    return count;
+    } 
+}
